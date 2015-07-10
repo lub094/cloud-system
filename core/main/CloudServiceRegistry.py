6b43d8db18c17cdcabf2de447b5a964863339cf0
Lubo Kanev <lubokkanev@gmail.com>
2015-07-10 22:22:22 +0300

    Main method.

--- core/main/CloudServiceRegistry.py
+++ core/main/CloudServiceRegistry.py
@@ -1,14 +1,24 @@
-__author__ = 'Lubo'
-
-
 class CloudServiceRegistry:
-    def __init__(self):
-        self.binary_manager = None
-        self.task_manager = None
-        self.process_manager = None
-        self.runtime_manager = None
-        self.temporary_files_directory = ""
+
+    def __init__(self, binary_manager, task_manager,
+                 process_manager, runtime_manager, temporary_files_direcotry):
+        self.__binary_manager = binary_manager
+        self.__task_manager = task_manager
+        self.__process_manager = process_manager
+        self.__runtime_manager = runtime_manager
+        self.__temporary_files_directory = temporary_files_direcotry
 
     def get_temporary_files_directory(self):
-        return self.temporary_files_directory
+        return self.__temporary_files_directory
 
+    def get_task_manager(self):
+        return self.__task_manager
+
+    def get_binary_manager(self):
+        return self.__binary_manager
+
+    def get_process_manager(self):
+        return self.__process_manager
+
+    def get_runtime_manager(self):
+        return self.__runtime_manager
--- core/main/WindowsCloudServiceRegistry.py
+++ core/main/WindowsCloudServiceRegistry.py
@@ -1,9 +1,8 @@
 from CloudServiceRegistry import CloudServiceRegistry
 
-__author__ = 'Lubo'
-
 
 class WindowsCloudServiceRegistry(CloudServiceRegistry):
+
     @staticmethod
     def say_hello():
         print("Hello from the Windows cloud system!")
