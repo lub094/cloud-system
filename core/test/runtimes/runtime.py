09dd7c1f062e823ae010f5407454f94cc0e3be1d
Lubo Kanev <lubokkanev@gmail.com>
2015-08-30 17:42:53 +0300

    Added tests for the runtimes

---
+++ core/test/runtimes/runtime.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('X:\OneDrive\Programming\Code\Python\Projects\CloudSystem\cloud-system')
+
+import unittest
+from core.main.runtimes.runtime import Runtime
+
+
+class TestRuntime(unittest.TestCase):
+
+    id = 1
+    description = 'description'
+    owner = 'some owner'  # create a mock owner
+
+    def setUp(self):
+        self.runtime = Runtime(None, None, None)
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        self.runtime = Runtime(self.id, self.description, self.owner)
+
+        self.assertEqual(self.id, self.runtime.get_id())
+        self.assertEqual(self.description, self.runtime.get_description())
+        self.assertEqual(self.owner, self.runtime.get_owner())
+
+    def test_id_set_and_get(self):
+        self.runtime.set_id(self.id)
+        self.assertEqual(self.runtime.get_id(), self.id)
+
+    def test_description_set_and_get(self):
+        self.runtime.set_description(self.description)
+        self.assertEqual(self.runtime.get_description(), self.description)
+
+    def test_owner_set_and_get(self):
+        self.runtime.set_owner(self.owner)
+        self.assertEqual(self.runtime.get_owner(), self.owner)
+
+    def test_str(self):
+        self.assertEqual(str(self.runtime), 'None None None')
+
+unittest.main()
